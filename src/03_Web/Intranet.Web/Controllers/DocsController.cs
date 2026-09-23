cfb5361ac0d4f3881de7b8efd83b449f8fd0e14b
Felipe Ostos <felipe.ostosb@istpargentina.edu.pe>
2026-09-22 19:53:42 -0500

    feat(docs): integrate interactive PO requirement form and A4 PDF generator into intranet

--- src/03_Web/Intranet.Web/Controllers/DocsController.cs
+++ src/03_Web/Intranet.Web/Controllers/DocsController.cs
@@ -98,6 +98,15 @@ public class DocsController : ModuloBaseController
         return View(query.OrderByDescending(t => t.FechaPublicacion).ToList());
     }
 
+    [HttpGet("ProductOwner")]
+    [HttpGet("FichaPO")]
+    [HttpGet("Requerimientos")]
+    public IActionResult ProductOwner()
+    {
+        ViewData["Title"] = "Ficha Rápida de Requerimiento PO & Generador A4";
+        return View();
+    }
+
     [HttpPost("Agregar")]
     [ValidateAntiForgeryToken]
     public IActionResult Agregar(string titulo, string descripcion, string youtubeUrl, string moduloCodigo, string autor, string nivel)
